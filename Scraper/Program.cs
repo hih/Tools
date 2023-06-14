20616197a6b39cb7c925c8aeb5b66abfee2fea32
hih <james_super1414@hotmail.co.uk>
2023-06-14 22:27:19 +0100

    Parse the html of the page

--- Scraper/Program.cs
+++ Scraper/Program.cs
@@ -1,4 +1,5 @@
 ﻿using HtmlAgilityPack;
+using Scraper.Models;
 using Scraper.Utils;
 using System;
 using System.Collections.Generic;
@@ -13,12 +14,26 @@ namespace Scraper
 			var url = "https://www.totaljobs.com/job/net-developer/adria-solutions-job100571702?TemplateType=Standard";
 
 			var downloader = new Downloader();
+			var parser = new Parser();
 
 			string content = await downloader.DownloadPageAsync(url);
 
 			if (content != null)
 			{
-				Console.WriteLine(content.Substring(0, 100));
+				//Console.WriteLine(content.Substring(0, 100));
+
+				var jobPosting = parser.ParseJobPosting(content);
+
+				if (jobPosting != null)
+				{
+					Console.WriteLine($"Position: {jobPosting.Position}");
+					Console.WriteLine($"Body: {jobPosting.Body}");
+					Console.WriteLine($"Location: {jobPosting.Location}");
+					Console.WriteLine($"Company: {jobPosting.Company}");
+					Console.WriteLine($"SalaryFrom: {jobPosting.SalaryFrom}");
+					Console.WriteLine($"SalaryTo: {jobPosting.SalaryTo}");
+					Console.WriteLine($"JobType: {jobPosting.JobType}");
+				}
 			}
 		}
 	}
---
+++ Scraper/Utils/Parser.cs
@@ -0,0 +1,70 @@
+﻿using HtmlAgilityPack;
+using Scraper.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scraper.Utils
+{
+	public class Parser
+	{
+		public JobPosting ParseJobPosting(string htmlContent)
+		{
+			var htmlDoc = new HtmlDocument();
+			htmlDoc.LoadHtml(htmlContent);
+
+			// TODO: Move config out to allow for multiple job boards
+			var positionXPath = "/html/body/div[2]/div[5]/div[1]/div[1]/div/div/div[1]/div/div/div[1]/div/h1";
+			var bodyXPath = "/html/body/div[2]/div[5]/div[1]/div[1]/div/div/div[3]/div/div";
+			var locationXPath = "/html/body/div[2]/div[5]/div[1]/div[1]/div/div/div[1]/div/div/div[1]/section/ul[1]/li[1]/div/a";
+			var salaryXPath = "/html/body/div[2]/div[5]/div[1]/div[1]/div/div/div[1]/div/div/div[1]/section/ul[1]/li[2]/div";
+			var jobTypeXPath = "/html/body/div[2]/div[5]/div[1]/div[1]/div/div/div[1]/div/div/div[1]/section/ul[2]/li[2]/div";
+			var companyXPath = "//*[@id=\"companyJobsLink\"]";
+
+			var positionNode = htmlDoc.DocumentNode.SelectSingleNode(positionXPath);
+			var bodyNode = htmlDoc.DocumentNode.SelectSingleNode(bodyXPath);
+			var locationNode = htmlDoc.DocumentNode.SelectSingleNode(locationXPath);
+			var salaryNode = htmlDoc.DocumentNode.SelectSingleNode(salaryXPath);
+			var jobTypeNode = htmlDoc.DocumentNode.SelectSingleNode(jobTypeXPath);
+			var companyNode = htmlDoc.DocumentNode.SelectSingleNode(companyXPath);
+
+			var salaryText = salaryNode.InnerText.Trim().Split(" - ");
+			var salaryFrom = Decimal.Parse(salaryText[0].Replace("&#163;", ""));
+			var salaryto = Decimal.Parse(salaryText[1].Replace("&#163;", ""));
+
+			JobType jobType;
+
+			switch (jobTypeNode.InnerText.Trim())
+			{
+				case "Contract":
+					jobType = JobType.Contract;
+					break;
+				case "PartTime":
+					jobType = JobType.PartTime;
+					break;
+				case "Permanent":
+				case "Fulltime":
+					jobType = JobType.FullTime;
+					break;
+				default:
+					jobType = JobType.FullTime;
+					break;
+			}
+
+			return new JobPosting
+			{
+				Position = positionNode.InnerText.Trim(),
+				Body = bodyNode.InnerText.Trim(),
+				Location = locationNode.InnerText.Trim(),
+				Company = companyNode.InnerText.Trim(),
+				SalaryTo = salaryto,
+				SalaryFrom = salaryFrom,
+				JobType = jobType,
+			};
+		}
+	}
+}
