a1fc3fbfaacd2f8ba2e7a31a8ded59a5e31bb57f
hih <james_super1414@hotmail.co.uk>
2023-06-14 22:27:45 +0100

    Parse the html of the page

---
+++ Scraper/Models/JobPosting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scraper.Models
+{
+    public class JobPosting
+	{
+        public string Position { get; set; }
+        public string Body { get; set; }
+        public string Location { get; set; }
+        public string Company { get; set; }
+
+        public decimal SalaryFrom { get; set; }
+        public decimal SalaryTo { get; set; }
+
+        public JobType JobType { get; set; }
+    }
+
+	public enum JobType
+    {
+        Contract = 0,
+        PartTime = 1,
+        FullTime = 2,
+    }
+}
