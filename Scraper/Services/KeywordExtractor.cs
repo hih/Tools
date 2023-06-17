aef67086a78ead61d6866dbb7155a7e290cd7a7a
hih <james_super1414@hotmail.co.uk>
2023-06-17 15:58:48 +0100

    Update keywords

--- Scraper/Services/KeywordExtractor.cs
+++ Scraper/Services/KeywordExtractor.cs
@@ -5,7 +5,15 @@
 		// TODO: NLP
 		private List<string> _keywords = new List<string>()
 		{
-			"C#", ".NET", "ASP.NET", "JavaScript", "React", "Angular", "SQL", "Python", "Java"
+			".NET 7", ".NET Core", ".NET Developer", "Agile", "Scrum", "Kanban", "Ajax", "Angular",
+			"API", "Web Api", "MVC", "VB", "VB.NET", "AWS", "AWS Lambda", "Azure", "Azure SQL", "Azure Devops",
+			"Bash", "BDD", "TDD", "Blazor", "CI", "CD", "Clean code", "HTML", "HTML5", "CSS", "JS", "JavaScript",
+			"C#", ".NET", "ASP.NET", "JavaScript", "React", "Angular", "SQL", "Python", "Java",
+			"Degree", "Devops", "Docker", "DRY", "SOLID", "Dynamo DB", "EF Core", "EntityFramework",
+			"Entity Framework", "GCP", "AWS", "Git", "Github", "Gitlab", "GO", "Golang", "Jira", "JSON",
+			"JQuery", "Kubernetes", "LINQ", "Linux", "Shell", "Bash", "Microservices", "MongoDB", "MS Office",
+			"MVC", "Next.Js", "Prism", "Problem solving", "TSQL", "T-SQL", "Visual Studio", "VueJS", "Vue.Js",
+			"Vue Js", "WPF", "Webforms"
 		};
 
 		public List<string> ExtractKeywords(string text)
