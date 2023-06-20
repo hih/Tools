84fa9511fa7212dd4bc30f003dd42b43eac397f3
hih <james_super1414@hotmail.co.uk>
2023-06-20 21:36:43 +0100

    Remove country code from Locations

---
+++ Tools/Data/Migrations/20230620203524_remove-country-code-from-locations.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tools.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class removecountrycodefromlocations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountryCode",
+                table: "Locations");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CountryCode",
+                table: "Locations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
